3ad8b4cddd6a48ee0e9a17077daf788a71fc7b14
Edy Molina <36119589+edymol@users.noreply.github.com>
2023-12-30 17:04:12 -0800

    add: currency-exchange-service

---
+++ currency-exchange-service/src/main/java/xyz/edydev/microservice/currencyexchangeservice/CurrencyExchangeRepository.java
@@ -0,0 +1,7 @@
+package xyz.edydev.microservice.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository extends JpaRepository <CurrencyExchange, Long>{
+    CurrencyExchange findByFromAndTo(String from, String to);
+}
